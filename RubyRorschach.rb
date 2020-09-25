ca84da3ee33737b53ca97b3fbb51acccb8142bd3
GDawg78 <me@gavinsimms.com>
2020-09-25 11:21:18 +1000

    Created start of welcome menu and mvc folder structure

--- RubyRorschach.rb
+++ RubyRorschach.rb
@@ -1,4 +1,12 @@
-puts "Is this working? "
-answer = gets.chomp
+puts "Welcome to the Ruby Rorschach Test" "\n"
+print "
+                         _                _     
+                        | |              | |    
+ _ __ ___  _ __ ___  ___| |__   __ _  ___| |__  
+| '__/ _ \| '__/ __|/ __| '_ \ / _` |/ __| '_ \ 
+| | | (_) | |  \__ \ (__| | | | (_| | (__| | | |
+|_|  \___/|_|  |___/\___|_| |_|\__,_|\___|_| |_|  
 
-puts answer
+"
+puts "Please select your option from the menu below: "
+                        
