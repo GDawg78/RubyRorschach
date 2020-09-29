484ec9482f36af642756dcebcf543bd75f345d24
GDawg78 <me@gavinsimms.com>
2020-09-29 17:05:12 +1000

    created block to interpret results of code

--- RubyRorschach.rb
+++ RubyRorschach.rb
@@ -3,7 +3,7 @@ require 'colorize'
 require_relative "./models/Plates.rb"
 require_relative "./models/bananna.rb"
 
-prompt = TTY::Prompt.new
+$prompt = TTY::Prompt.new
 
 puts "Welcome to the Ruby Rorschach Test\n"
 puts "
@@ -22,7 +22,7 @@ puts "
 ".colorize(:blue)
 
 puts "Please select your option from the menu below: "
-
+$prompt = TTY::Prompt.new
 $answer_values = {}
 
 def evaluate_answer(answer)
@@ -38,60 +38,61 @@ def evaluate_answer(answer)
     end
 end
 
+
 def take_test
     system ("clear")
     puts "Please view image and select the best response for you"
     plates1
-    answer_one = prompt.select("Choose", ["A: A Bat", "B: Two people", "C: Pincers of a crab", "D: A pelvis"]) # keep that format
+    answer_one = $prompt.select("Choose", ["A: A Bat", "B: Two people", "C: Pincers of a crab", "D: A pelvis"]) # keep that format
     evaluate_answer(answer_one[0])
     puts $answer_values["Paranoid"]
     system ("clear")
     puts "Please view image and select the best response for you"
     plates2
-    answer_two = prompt.select("Choose", ["A: Two clowns", "B: A bug somebody stepped on", "C: A bursting bomb", "D: A bloody spinal column"])
+    answer_two = $prompt.select("Choose", ["A: Two clowns", "B: A bug somebody stepped on", "C: A bursting bomb", "D: A bloody spinal column"])
     evaluate_answer(answer_two[0])
     system ("clear")
     puts "Please view image and select the best response for you"
     plates3
-    answer_three = prompt.select("Choose", ["A: Two men", "B: A butterfly", "C: Part of my body", "D: Two birds"])
+    answer_three = $prompt.select("Choose", ["A: Two men", "B: A butterfly", "C: Part of my body", "D: Two birds"])
     evaluate_answer(answer_three[0])
     puts $answer_values
     system ("clear")
     puts "Please view image and select the best response for you"
     plates4
-    answer_four = prompt.select("Choose", ["A: Head of animal", "B: Lungs and chest", "C: An X-ray picture", "D: A nasty mess"])
+    answer_four = $prompt.select("Choose", ["A: Head of animal", "B: Lungs and chest", "C: An X-ray picture", "D: A nasty mess"])
     evaluate_answer(answer_four[0])
     puts $answer_values
     system ("clear")
     puts "Please view image and select the best response for you"
     plates5
-    answer_five = prompt.select("Choose", ["A: A bat or butterfly", "B: An alligator's head", "C: A smashed body", "D: A fan dancer"])
+    answer_five = $prompt.select("Choose", ["A: A bat or butterfly", "B: An alligator's head", "C: A smashed body", "D: A fan dancer"])
     evaluate_answer(answer_five[0])
     puts $answer_values
     system ("clear")
     puts "Please view image and select the best response for you"
     plates6
-    answer_six = prompt.select("Choose", ["A: Sex organs", "B: A fur rug", "C: A turtle", "D: Two kings' crowns"])
+    answer_six = $prompt.select("Choose", ["A: Sex organs", "B: A fur rug", "C: A turtle", "D: Two kings' crowns"])
     evaluate_answer(answer_six[0])
     system ("clear")
     puts "Please view image and select the best response for you"
-            plates7
-    answer_seven = prompt.select("Choose", ["A: Two women talking", "B: Bookends", "C: A map", "D: Lamb's tails, or feathers"])
+    plates7
+    answer_seven = $prompt.select("Choose", ["A: Two women talking", "B: Bookends", "C: A map", "D: Lamb's tails, or feathers"])
     evaluate_answer(answer_seven[0])
     system ("clear")
     puts "Please view image and select the best response for you"
     plates8
-    answer_eight = prompt.select("Choose", ["A: Parts of my body", "B: A horseshoe crab", "C: Fire and ice, life and death", "D: Two animals"])
+    answer_eight = $prompt.select("Choose", ["A: Parts of my body", "B: A horseshoe crab", "C: Fire and ice, life and death", "D: Two animals"])
     evaluate_answer(answer_eight[0])
     system ("clear")
     puts "Please view image and select the best response for you"
     plates9
-    answer_nine = prompt.select("Choose", ["A: Sea horses, or lobsters", "B: Flowers or underwater vegetation", "C: Deer or horns of a deer", "D: Two people-witches or Santa Clauses"])
+    answer_nine = $prompt.select("Choose", ["A: Sea horses, or lobsters", "B: Flowers or underwater vegetation", "C: Deer or horns of a deer", "D: Two people-witches or Santa Clauses"])
     evaluate_answer(answer_nine[0])
     system ("clear")
     puts "Please view image and select the best response for you"
     plates10
-    answer_ten = prompt.select("Choose", ["A: A Chinese print", "B: Nothing at all", "C: Spider, caterpillars, crabs and insects", "D: Two people"])
+    answer_ten = $prompt.select("Choose", ["A: A Chinese print", "B: Nothing at all", "C: Spider, caterpillars, crabs and insects", "D: Two people"])
     evaluate_answer(answer_ten[0])
     system ("clear")
 end
@@ -99,21 +100,23 @@ end
 def app_loop
     while true
         $answer_values = {"Common, Well adjusted" => 0, "Psychopathy" => 0, "Psychological disturbance" => 0, "Paranoid" => 0 }
-        prompt = TTY::Prompt.new
-        answer = prompt.select("Choose your option?", ["Take Test", "Exit"])
+        answer = $prompt.select("Choose your option?", ["Take Test", "Exit"])
         case answer
         when "Exit"
             puts "thankyou, goodbye"
             exit
         when "Take Test"
             take_test
+            highest_score_keys = [] #array from hash keys dependent on highest score
+            highest_score_value = $answer_values.values.max #grabs the first found highest score... 
+            $answer_values.each do |k,v|
+                if v == highest_score_value then # this checks to see if there are equally highest scored values
+                    highest_score_keys.push(k)
+                end
+            end
+            puts 
         end
     end
 end    
 
-
-app_loop
-
-
-
-                      
+app_loop 
