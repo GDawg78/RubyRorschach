b9fc10eb9251cf627ac23fb77eef7a95606c653a
GDawg78 <me@gavinsimms.com>
2020-09-29 13:38:42 +1000

    refactoring and applying DRY, changing repetitive text to functions

--- RubyRorschach.rb
+++ RubyRorschach.rb
@@ -25,7 +25,7 @@ puts "Please select your option from the menu below: "
 
 def app_loop
     while true
-        answer_values = {"Paranoid" => 0, "Sane" => 0, "Insane" => 0, "Happy" => 0 }
+        answer_values = {"Common, Well adjusted" => 0, "Psychopathy" => 0, "Psychological disturbance" => 0, "Paranoid" => 0 }
         prompt = TTY::Prompt.new
         answer = prompt.select("Choose your option?", ["Take Test", "Exit"])
         case answer
@@ -39,55 +39,144 @@ def app_loop
             answer_one = prompt.select("Choose", ["A: A Bat", "B: Two people", "C: Pincers of a crab", "D: A pelvis"]) # keep that format
             case answer_one[0]
             when "A"
-                answer_values["Paranoid"] += 1
+                answer_values["Common, Well adjusted"] += 1
             when "B"
-                answer_values["Sane"] += 1
+                answer_values["Psychopathy"] += 1
             when "C"
-                answer_values["Insane"] += 1
+                answer_values["Psychological disturbance"] += 1
             when "D"
-                answer_values["Happy"] += 1
+                answer_values["Paranoid"] += 1
             end
             puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates2
             answer_two = prompt.select("Choose", ["A: Two clowns", "B: A bug somebody stepped on", "C: A bursting bomb", "D: A bloody spinal column"])
+            case answer_two[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates3
             answer_three = prompt.select("Choose", ["A: Two men", "B: A butterfly", "C: Part of my body", "D: Two birds"])
+            case answer_three[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates4
             answer_four = prompt.select("Choose", ["A: Head of animal", "B: Lungs and chest", "C: An X-ray picture", "D: A nasty mess"])
+            case answer_four[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates5
             answer_five = prompt.select("Choose", ["A: A bat or butterfly", "B: An alligator's head", "C: A smashed body", "D: A fan dancer"])
+            case answer_five[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates6
             answer_six = prompt.select("Choose", ["A: Sex organs", "B: A fur rug", "C: A turtle", "D: Two kings' crowns"])
+            case answer_six[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates7
             answer_seven = prompt.select("Choose", ["A: Two women talking", "B: Bookends", "C: A map", "D: Lamb's tails, or feathers"])
+            case answer_seven[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates8
             answer_eight = prompt.select("Choose", ["A: Parts of my body", "B: A horseshoe crab", "C: Fire and ice, life and death", "D: Two animals"])
+            case answer_eight[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates9
             answer_nine = prompt.select("Choose", ["A: Sea horses, or lobsters", "B: Flowers or underwater vegetation", "C: Deer or horns of a deer", "D: Two people-witches or Santa Clauses"])
+            case answer_one[0]
+            when "A"
+                answer_values["Common, Well adjusted"] += 1
+            when "B"
+                answer_values["Psychopathy"] += 1
+            when "C"
+                answer_values["Psychological disturbance"] += 1
+            when "D"
+                answer_values["Paranoid"] += 1
+            end
+            puts answer_values
             system ("clear")
             puts "Please view image and select the best response for you"
             plates10
-            answer_ten = prompt.select("Choose", ["A: A Chinese print", "B: Nothing at all", "C: Spider, caterpillars, crabs and insects", "D: Two people"])
+            
+        answer_ten = prompt.select("Choose", ["A: A Chinese print", "B: Nothing at all", "C: Spider, caterpillars, crabs and insects", "D: Two people"])
+        system ("clear")
         end
     end
-    system ("clear")
-
+    
     puts "
     
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
