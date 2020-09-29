e35fbaec5e41ee53cd28b125104e0f371898af76
GDawg78 <me@gavinsimms.com>
2020-09-29 13:07:48 +1000

    Finished creating answers for array

--- RubyRorschach.rb
+++ RubyRorschach.rb
@@ -36,7 +36,7 @@ def app_loop
             system ("clear")
             puts "Please view image and select the best response for you"
             plates1
-            answer_one = prompt.select("Choose", ["A: Bat", "B: Dog", "C: Mouse", "D: Rabbit"]) # keep that format
+            answer_one = prompt.select("Choose", ["A: A Bat", "B: Two people", "C: Pincers of a crab", "D: A pelvis"]) # keep that format
             case answer_one[0]
             when "A"
                 answer_values["Paranoid"] += 1
@@ -51,39 +51,39 @@ def app_loop
             system ("clear")
             puts "Please view image and select the best response for you"
             plates2
-            answer_two = prompt.select("Choose", %w(A B C D))
+            answer_two = prompt.select("Choose", ["A: Two clowns", "B: A bug somebody stepped on", "C: A bursting bomb", "D: A bloody spinal column"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates3
-            answer_three = prompt.select("Choose", %w(A B C D))
+            answer_three = prompt.select("Choose", ["A: Two men", "B: A butterfly", "C: Part of my body", "D: Two birds"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates4
-            answer_four = prompt.select("Choose", %w(A B C D))
+            answer_four = prompt.select("Choose", ["A: Head of animal", "B: Lungs and chest", "C: An X-ray picture", "D: A nasty mess"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates5
-            answer_five = prompt.select("Choose", %w(A B C D))
+            answer_five = prompt.select("Choose", ["A: A bat or butterfly", "B: An alligator's head", "C: A smashed body", "D: A fan dancer"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates6
-            answer_six = prompt.select("Choose", %w(A B C D))
+            answer_six = prompt.select("Choose", ["A: Sex organs", "B: A fur rug", "C: A turtle", "D: Two kings' crowns"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates7
-            answer_seven = prompt.select("Choose", %w(A B C D))
+            answer_seven = prompt.select("Choose", ["A: Two women talking", "B: Bookends", "C: A map", "D: Lamb's tails, or feathers"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates8
-            answer_eight = prompt.select("Choose", %w(A B C D))
+            answer_eight = prompt.select("Choose", ["A: Parts of my body", "B: A horseshoe crab", "C: Fire and ice, life and death", "D: Two animals"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates9
-            answer_nine = prompt.select("Choose", %w(A B C D))
+            answer_nine = prompt.select("Choose", ["A: Sea horses, or lobsters", "B: Flowers or underwater vegetation", "C: Deer or horns of a deer", "D: Two people-witches or Santa Clauses"])
             system ("clear")
             puts "Please view image and select the best response for you"
             plates10
-            answer_ten = prompt.select("Choose", %w(A B C D))
+            answer_ten = prompt.select("Choose", ["A: A Chinese print", "B: Nothing at all", "C: Spider, caterpillars, crabs and insects", "D: Two people"])
         end
     end
     system ("clear")
