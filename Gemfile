b7eabbb4cb941da9cf5902faa6ceabb57fd39809
GDawg78 <me@gavinsimms.com>
2020-09-29 12:09:49 +1000

    Created Array writeback

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "colorize", "~> 0.8.1"
+
+gem "rspec", "~> 3.9"
+
+gem "tty-prompt", "~> 0.22.0"
--- RubyRorschach.rb
+++ RubyRorschach.rb
@@ -21,58 +21,70 @@ puts "
 
 ".colorize(:blue)
 
-# self.go
-
 puts "Please select your option from the menu below: "
 
 def app_loop
-    prompt = TTY::Prompt.new
-    answer = prompt.select("Choose your option?", %w(Take\ Test Exit))
-    case answer
-    when "Exit"
-        puts "thankyou, goodbye"
-        exit
-    when "Take\ Test"
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates1
-        answer_one = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates2
-        answer_two = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates3
-        answer_three = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates4
-        answer_four = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates5
-        answer_five = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates6
-        answer_six = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates7
-        answer_seven = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates8
-        answer_eight = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates9
-        answer_nine = prompt.select("Choose", %w(A B C D))
-        system ("clear")
-        puts "Please view image and select the best response for you"
-        plates10
-        answer_ten = prompt.select("Choose", %w(A B C D))
+    while true
+        answer_values = {"Paranoid" => 0, "Sane" => 0, "Insane" => 0, "Happy" => 0 }
+        prompt = TTY::Prompt.new
+        answer = prompt.select("Choose your option?", ["Take Test", "Exit"])
+        case answer
+        when "Exit"
+            puts "thankyou, goodbye"
+            exit
+        when "Take Test"
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates1
+            answer_one = prompt.select("Choose", ["A: Bat", "B: Dog", "C: Mouse", "D: Rabbit"]) # keep that format
+            case answer_one[0]
+            when "A"
+                answer_values["Paranoid"] += 1
+            when "B"
+                answer_values["Sane"] += 1
+            when "C"
+                answer_values["Insane"] += 1
+            when "D"
+                answer_values["Happy"] += 1
+            end
+            puts answer_values
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates2
+            answer_two = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates3
+            answer_three = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates4
+            answer_four = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates5
+            answer_five = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates6
+            answer_six = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates7
+            answer_seven = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates8
+            answer_eight = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates9
+            answer_nine = prompt.select("Choose", %w(A B C D))
+            system ("clear")
+            puts "Please view image and select the best response for you"
+            plates10
+            answer_ten = prompt.select("Choose", %w(A B C D))
+        end
     end
     system ("clear")
 
@@ -91,6 +103,7 @@ def app_loop
 
 
     "
+
 end
 
 app_loop
